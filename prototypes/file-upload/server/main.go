5b39f34145f2d067dfdec47328694ca40f294e55
Josh Heinrichs <joshiheinrichs@gmail.com>
2015-08-31 19:29:18 -0600

    Split function in upload prototype

--- prototypes/file-upload/server/main.go
+++ prototypes/file-upload/server/main.go
@@ -10,6 +10,7 @@ import (
 	"crypto/md5"
 	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"github.com/gorilla/mux"
 	"github.com/nu7hatch/gouuid"
 	"github.com/gographics/imagick/imagick"
@@ -20,8 +21,66 @@ const (
 	MAX_HEIGHT = 200
 )
 
+func saveImage(file multipart.File) error {
+	u4, err := uuid.NewV4()
+	if err != nil {
+	    fmt.Println("error:", err)
+	    return err
+	}
+
+    blob, err := ioutil.ReadAll(file)
+    if err != nil {
+    	fmt.Println("error:", err)
+    	return err
+    }
+
+    mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	err = mw.ReadImageBlob(blob)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	width := mw.GetImageWidth()
+	height := mw.GetImageHeight()
+
+	var newWidth, newHeight uint
+	if width > MAX_WIDTH {
+		newWidth = MAX_WIDTH
+		newHeight = uint(float32(height) / float32(width / MAX_WIDTH))
+	}
+	if newHeight > MAX_HEIGHT {
+		newHeight = MAX_HEIGHT
+		newWidth = uint(float32(width) / float32(height / MAX_HEIGHT))
+	}
+
+	// Resize the image using the Lanczos filter
+	// The blur factor is a float, where > 1 is blurry, < 1 is sharp
+	err = mw.ResizeImage(newWidth, newHeight, imagick.FILTER_LANCZOS, 1)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	err = mw.SetImageCompressionQuality(70)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	err = mw.WriteImage("app/images/" + u4.String() + ".jpg")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
 // upload logic
-func upload(w http.ResponseWriter, r *http.Request) {
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println("method:", r.Method)
     if r.Method == "GET" {
         crutime := time.Now().Unix()
@@ -29,7 +88,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
         io.WriteString(h, strconv.FormatInt(crutime, 10))
         token := fmt.Sprintf("%x", h.Sum(nil))
 
-        t, _ := template.ParseFiles("upload.gtpl")
+        t, _ := template.ParseFiles("uploadHandler.gtpl")
         t.Execute(w, token)
     } else {
         r.ParseMultipartForm(32 << 20)
@@ -39,62 +98,13 @@ func upload(w http.ResponseWriter, r *http.Request) {
             return
         }
         defer file.Close()
-
-        fmt.Fprintf(w, "%v", handler.Header)
-
-        u4, err := uuid.NewV4()
-		if err != nil {
-		    fmt.Println("error:", err)
-		    return
-		}
-
-        blob, err := ioutil.ReadAll(file)
+        err = saveImage(file)
         if err != nil {
-        	fmt.Println("error:", err)
+        	fmt.Println(err)
         	return
         }
 
-        mw := imagick.NewMagickWand()
-		defer mw.Destroy()
-
-		err = mw.ReadImageBlob(blob)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		width := mw.GetImageWidth()
-		height := mw.GetImageHeight()
-
-		var newWidth, newHeight uint
-		if width > MAX_WIDTH {
-			newWidth = MAX_WIDTH
-			newHeight = uint(float32(height) / float32(width / MAX_WIDTH))
-		}
-		if newHeight > MAX_HEIGHT {
-			newHeight = MAX_HEIGHT
-			newWidth = uint(float32(width) / float32(height / MAX_HEIGHT))
-		}
-
-		// Resize the image using the Lanczos filter
-		// The blur factor is a float, where > 1 is blurry, < 1 is sharp
-		err = mw.ResizeImage(newWidth, newHeight, imagick.FILTER_LANCZOS, 1)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		err = mw.SetImageCompressionQuality(70)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		err = mw.WriteImage("app/images/" + u4.String() + ".jpg")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+        fmt.Fprintf(w, "%v", handler.Header)
     }
 }
 
@@ -104,7 +114,7 @@ func main() {
     defer imagick.Terminate()
 
     r := mux.NewRouter()
-    r.HandleFunc("/upload", upload)
+    r.HandleFunc("/upload", uploadHandler)
     r.PathPrefix("/").Handler(http.FileServer(http.Dir("app/")))
 
     http.Handle("/", r)
