3f16278747f9d4643ba789df3dde379ca05e51d2
Josh Heinrichs <joshiheinrichs@gmail.com>
2016-01-08 12:02:35 -0600

    Add checkbox implementation

--- server/field/checkboxes.go
+++ server/field/checkboxes.go
@@ -8,9 +8,15 @@ type Checkbox struct {
 }
 
 func (checkboxes *Checkboxes) Validate() error {
+	if checkboxes == nil {
+		return ErrMissingValue
+	}
 	return nil
 }
 
 func (checkboxes *Checkboxes) IsEmpty() bool {
+	if checkboxes == nil {
+		return true
+	}
 	return false
 }
