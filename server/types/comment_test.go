d862d56b2a73a7b5108db2bd9fc7cf358f528330
Josh Heinrichs <joshiheinrichs@gmail.com>
2016-03-18 13:52:43 -0600

    Add comment validation test

---
+++ server/types/comment_test.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommentValidate(t *testing.T) {
+	// Testing empty string
+	comment := Comment{}
+	assert.Error(t, comment.Validate())
+
+	// Testing minimum string length
+	comment.Comment = "a"
+	assert.NoError(t, comment.Validate())
+
+	// Testing maximum string length
+	maxString := ""
+	for i := 0; i < MAX_COMMENT_LENGTH; i++ {
+		maxString += "a"
+	}
+	comment.Comment = maxString
+	assert.NoError(t, comment.Validate())
+	assert.Equal(t, maxString, comment.Comment)
+
+	// Testing trimming
+	comment.Comment = "   " + maxString + "   "
+	assert.NoError(t, comment.Validate())
+	assert.Equal(t, maxString, comment.Comment)
+
+	// Testing string that is too large
+	comment.Comment = maxString + "a"
+	assert.Error(t, comment.Validate())
+}
