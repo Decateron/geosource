0ee528b350ccc703061208325b9ae2d0a97da412
Josh Heinrichs <joshiheinrichs@gmail.com>
2016-03-17 20:40:53 -0600

    Add subscription transactions

--- server/transactions/subscriptions.go
+++ server/transactions/subscriptions.go
@@ -1,13 +1,51 @@
 package transactions
 
-import "errors"
-
-func AddSubscription(requester, channelname string) error {
-	return errors.New("function has not yet been implemented.")
+// Returns true if the requester can view subscriptions.
+func CanViewSubscriptions(requester, userID string) (bool, error) {
+	return requester == userID, nil
 }
 
-// func GetSubscriptions
+func CanModifySubscriptions(requester, userID string) (bool, error) {
+	return requester == userID, nil
+}
 
-func RemoveSubscription(requester, channelname string) error {
-	return errors.New("function has not yet been implemented.")
+func AddSubscription(requester, userID, channelname string) error {
+	permission, err := CanModifySubscriptions(requester, userID)
+	if err != nil {
+		return err
+	} else if !permission {
+		return ErrInsufficientPermission
+	}
+	return db.Exec("INSERT INTO user_subscriptions (us_userid, us_channelname) VALUES (?, ?)", requester, channelname).Error
+}
+
+func GetSubscriptions(requester, userID string) ([]string, error) {
+	permission, err := CanViewSubscriptions(requester, userID)
+	if err != nil {
+		return nil, err
+	} else if !permission {
+		return nil, ErrInsufficientPermission
+	}
+	var subscriptions []struct {
+		PostID string `gorm:"column:us_channelname"`
+	}
+	err = db.Table("user_subscriptions").Where("us_userid = ?", userID).Find(&subscriptions).Error
+	if err != nil {
+		return nil, err
+	}
+	channelnames := make([]string, len(subscriptions))
+	for i, subscription := range subscriptions {
+		channelnames[i] = subscription.PostID
+	}
+	return channelnames, nil
+}
+
+func RemoveSubscription(requester, userID, channelname string) error {
+	permission, err := CanModifySubscriptions(requester, userID)
+	if err != nil {
+		return err
+	} else if !permission {
+		return ErrInsufficientPermission
+	}
+	return db.Exec("DELETE FROM user_subscriptions WHERE us_userid = ? AND us_channelname = ?", requester, channelname).Error
 }
